475ef606adad31d48bbc3e26e0edfcbdd852bdf5
a9sk <920a9sk42f76c765@proton.me>
2024-05-12 13:33:34 +0200

    functions rename added

--- obf.py
+++ obf.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import random
+import string
+#import ast #? Abstract Syntax Tree
+import re
 
 def help():
     print("""
@@ -43,7 +47,35 @@ def banner():
 
             Mushroomic v1.0.0 @a9sk
 """)
+def functions(code):
+    print('[*] Searching for functions, all will be renamed')
+    
+    used_names=set()
+    name_mapping={}
+    pattern = r'def\s+(\w+)\s*\('
+    matches = re.findall(pattern, code)
 
+    def random_name():
+        while True:
+            new_name = ''.join(random.choices(string.ascii_letters, k=10)) # Generate random names (might be changed to just strange names)
+            if new_name not in used_names:
+                used_names.add(new_name)
+                return new_name 
+
+    for old_name in matches:
+        new_name = random_name()
+        name_mapping[old_name] = new_name
+        modified_functions_code = code.replace(f'def {old_name}', f'def {new_name}')
+        print(old_name)
+        print(new_name)
+
+    for old_name, new_name in name_mapping.items():
+        print(old_name)
+        print(new_name)
+        modified_functions_code = re.sub(r'\b' + re.escape(old_name) + r'\b(?=\()', new_name, modified_functions_code)
+
+
+    return modified_functions_code
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -55,23 +87,38 @@ if __name__ == '__main__':
     
     if '-c' in args: 
         index_code = args.index('-c')
-        original_name = args[index_code + 1]
-        file_name, file_extension = original_name.rsplit('.', 1)
-        if file_extension != 'py':
-            print('[!] The obfuscation only works with python scripts for now')
-            print('[*] Pass a .py script as an argument after -c')
-            exit()
-        if os.path.exists(original_name):
-            print('[*] File found in the directory')
-            with open(original_name, 'r') as original:
-                original_code=original.read()
-                print('[*] File opened correctly')
+        if index_code + 1 < len(args):
+            original_name = args[index_code + 1]
+            file_name, file_extension = original_name.rsplit('.', 1)
+            if file_extension != 'py':
+                print('[!] The obfuscation only works with python scripts for now')
+                print('[*] Pass a .py script as an argument after -c')
+                exit()
+            if os.path.exists(original_name):
+                print('[*] File found in the directory')
+                with open(original_name, 'r') as original:
+                    original_code=original.read()
+                    print('[*] File opened correctly')
+            else:
+                    print("[!] The file does not exist")
+                    exit()     
         else:
-                print("[!] The file does not exist")
-                exit()     
+            print("[!] Missing a valid file name after '-c' flag")
+            exit()
     else:
         print("[!] Missing '-c' flag followed by a file name.")
         exit()
+
+    if '-a' in args or '-f' in args or '-b' in args or '-v' in args or '-e' in args:
+        print("")
+    else:
+        print("[!] None of the flags (-a, -f, -b, -v, -e) are present.")
+        exit()
+
+    if '-f' in args:
+        print('[*] Obfuscating function names')
+        modified_code=functions(original_code)
+
 
     #! Should be done only after the whole code is obfuscated 
 
@@ -80,7 +127,7 @@ if __name__ == '__main__':
     with open(out, "w") as output:
         print('[*] Saving the obfuscated code in')
         print(out)
-        output.write(modified_code)
+        output.write(str(modified_code))
 
         
 
